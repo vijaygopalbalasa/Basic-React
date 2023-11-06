0d4b396549d6fb4f30bcfa5e364fc44e237ae6a3
Vijaygopal Balasa <balasavijaytheking@gmail.com>
2023-11-07 00:06:26 +0530

    Learned Maps and Memo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import Button from "./Button.jsx";
-import React from "react";
+import React, {useState} from "react";
+import Text from "./Text.js";
 
 const App = () => {
-    return <Button>
-        Click  Me
-    </Button>
+     const [dataf, setDataf] = useState();
+
+    const data = [
+        {id: 'a', Text: "text1"},
+        {id: 'b', Text: "text2"},
+        {id: 'c', Text: "text3"},
+        {id: 'd', Text: "text4"},
+        {id: 'e', Text: "text5"},
+    ]
+    return <>
+        {data.map((item) => <Text>{item.Text}</Text>)}
+    </>
 
 }
 
